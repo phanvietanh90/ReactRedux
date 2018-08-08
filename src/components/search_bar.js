1f7cd06ed22e03772259a1b91c62eaa45ac3890a
Phan Viet Anh <anh.phan@ascendcorp.com>
2018-08-08 18:22:02 +0700

    some first steps

---
+++ src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return < input onChange={(event) => console.log(event.target.value)} />;
+    }
+}
+
+// the same with:
+// const SearchBar = () => {
+//     return < input />;
+// };
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyAaL1y4eNFhCPKOJ2ZfppHJHJRVWoQ4n6M';
+const App = () => {
+    return (
+    <div> 
+     <SearchBar />       
+    </div>
+    );
+}
+ReactDOM.render(<App/>, document.querySelector('.container'));
