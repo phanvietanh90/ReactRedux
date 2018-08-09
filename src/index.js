eeb98c341f5c0d0e4c4dddfb8915f27ed6c6c5a8
Phan Viet Anh <phanvietanh90@gmail.com>
2018-08-09 19:08:18 +0700

    able to select videos from list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyAaL1y4eNFhCPKOJ2ZfppHJHJRVWoQ4n6M';
-const App = () => {
-    return (
-    <div> 
-     <SearchBar />       
-    </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({ key: API_KEY, term: 'starwars' }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
+            </div>
+        );
+    }
 }
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
